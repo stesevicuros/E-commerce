f8a834ceeb388d5e42fa88ce9aceca980543abee
stesevicuros <stesevicu@gmail.com>
2022-05-26 17:24:31 +0200

    Create Models

---
+++ backend/models/Order.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const CardSchema = new mongoose.Schema(
+    {
+        userID: { type: String, required: true },
+        products: [
+            {
+                productId: { type: String },
+                quantity: { type: Number, default: 1 },
+            },
+        ],
+        amount: { type: Number, required: true },
+        address: { type: Object, required: true },
+        status: { type: String, Default: 'pending' },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Card', CardSchema);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,3 @@
 const router = require('express').Router();
 
-router.get('/usertest', (req, res) => {
-    res.send('User test is successful');
-});
-
-router.post('/userposttest', (req, res) => {
-    const username = req.body.username;
-    res.json('Your username is ' + username);
-});
-
 module.exports = router;

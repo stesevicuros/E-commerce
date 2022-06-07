9e21298d04662db59995e8f74fcac5047fc63478
stesevicuros <stesevicu@gmail.com>
2022-06-08 01:20:03 +0200

    Fix Responsivnes

--- src/responsive.js
+++ src/responsive.js
@@ -2,7 +2,7 @@ import { css } from 'styled-components';
 
 export const mobile = (props) => {
 	return css`
-		@media only screen and (max-width: 1080px) {
+		@media only screen and (max-width: 720px) {
 			${props}
 		}
 	`;

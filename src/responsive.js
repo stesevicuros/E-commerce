b1c3534a803b44514f652a22454077e9a341c748
stesevicuros <stesevicu@gmail.com>
2022-06-08 02:40:10 +0200

    Remove Responsivnes

--- src/responsive.js
+++ src/responsive.js
@@ -1,9 +1,9 @@
-import { css } from 'styled-components';
+// import { css } from 'styled-components';
 
-export const mobile = (props) => {
-	return css`
-		@media only screen and (max-width: 380px) {
-			${props}
-		}
-	`;
-};
+// export const mobile = (props) => {
+// 	return css`
+// 		@media only screen and (max-width: 380px) {
+// 			${props}
+// 		}
+// 	`;
+// };
